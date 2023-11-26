11bee3e7c845f37ea58cf95a4839108c232bd38d
NicoGGG <nicolas.guelfi@achilio.com>
2023-11-26 19:01:55 +0100

    ci: automatic build and push to private registry

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,9 +3,9 @@
 bind = "0.0.0.0:8000"
 workers = 3
 # Access log - records incoming HTTP requests
-accesslog = "/var/log/gunicorn.access.log"
+accesslog = "-"
 # Error log - records Gunicorn server goings-on
-errorlog = "/var/log/gunicorn.error.log"
+errorlog = "-"
 # Whether to send Django output to the error log
 capture_output = True
 # How verbose the Gunicorn error logs should be
